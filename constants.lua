5b2648254375d525c378040d5f386dbc76c84560
Sergey Shevtsov <job.shevtsov@ya.ru>
2022-05-09 13:01:23 +0300

    return

--- constants.lua
+++ constants.lua
@@ -4,3 +4,5 @@ Constants = {}
 Constants.CBR_URL = 'http://www.cbr.ru/scripts/XML_daily_eng.asp'
 Constants.TG_BASE_URL = string.format('https://api.telegram.org/bot%s',
                                       os.getenv('TG_BOT_TOKEN'))
+
+return Constants

f26434a9b0d4349f1613a3cc0dcaf7a7f592bf56
Sergey Shevtsov <job.shevtsov@ya.ru>
2022-05-10 13:49:37 +0300

    finish bot

--- bot.lua
+++ bot.lua
@@ -4,6 +4,9 @@ local Constants = require('constants')
 local inspect = require('inspect')
 local ltn12 = require('ltn12')
 local json = require('json')
+local xml2lua = require("xml2lua")
+local xmlhandler = require("xmlhandler.tree")
+
 
 ---@class bot
 bot = {}
@@ -20,33 +23,107 @@ end
 
 --- Requests single update from telegram api
 ---@param last_update_id number
----@return any
+---@return table
 bot.getupdate = function (last_update_id)
-   local params = {timeout = 60, limit = 1}
-   if not last_update_id == nil then
-       params.last_update_id = last_update_id
-   end
+    local params = {timeout = 60, limit = 1}
+    if not (last_update_id == nil) then
+        params.offset = last_update_id + 1
+    end
 
-   local query_string = helpers.parameterize(params)
-   print(query_string)
-   local url = helpers.join(Constants.TG_BASE_URL, '/getupdates')
-   print(url)
+    local query_string = helpers.parameterize(params)
+    local url = helpers.join(Constants.TG_BASE_URL, '/getupdates')
 
-   local response = {}
-   local _, status = http.request{
+    local response = {}
+    local _, status = http.request{
         method = 'GET',
         url = url .. query_string,
         sink = ltn12.sink.table(response)
-   }
+    }
 
-   local response_json = json:decode(table.concat(response))
-   if not response_ok(status, response_json) then
-       return nil
-   end
-   
-   local update = response_json.result[1]
-   return update
+    local response_json = json:decode(table.concat(response))
+    if not response_ok(status, response_json) then
+        return nil
+    end
+
+    local update = response_json.result[1]
+    return update
 end
 
 
-bot.getupdate()
+--- Send telegram message
+---@param chat_id number
+---@param text string
+---@return nil
+bot.send_message = function (chat_id, text)
+    local params = {chat_id = chat_id, text = text, parse_mode = 'MarkdownV2'}
+
+    local url = helpers.join(Constants.TG_BASE_URL, '/sendmessage')
+
+    local response = {}
+
+    http.request{
+        method = 'POST',
+        url = url,
+        headers = {['Content-Type'] = 'application/json'},
+        source = ltn12.source.string(json:encode(params)),
+        sink = ltn12.sink.table(response)
+    }
+end
+
+
+--- Get currencies from Central Bank API
+---@return table
+bot.get_currencies = function ()
+    local response = {}
+    local _, status = http.request{
+        method = 'GET',
+        url = Constants.CBR_URL,
+        sink = ltn12.sink.table(response)
+    }
+    return response
+end
+
+
+--- Parse required attributes from XML to table
+---@param xml string
+---@return table
+bot.parse_currencies = function (xml)
+    local currencies = {}
+    local parser = xml2lua.parser(xmlhandler)
+    parser:parse(xml)
+    for _, valute in ipairs(xmlhandler.root.ValCurs.Valute) do
+        local value = tonumber(valute.Value:gsub(',', '.'):format("%.2f"))
+        currencies[valute.CharCode] = {
+            name = valute.Name,
+            value = string.format("%.2f", (value / tonumber(valute.Nominal)))
+        }
+    end
+    return currencies
+end
+
+
+--- Dispatch message
+---@param message table
+---@return nil
+bot.process_message = function (message)
+    if message.text == '/help' then
+        bot.send_message(message.chat.id, Constants.CODES)
+        return
+    end
+
+    local cbr_response = bot.get_currencies()
+    if not cbr_response then
+        bot.send_message(message.chat.id, 'CBR not responding')
+        return
+    end
+
+    local currencies = bot.parse_currencies(table.concat(cbr_response))
+    local choice = currencies[message.text]
+    if not choice then
+        bot.send_message(message.chat.id, Constants.CODES)
+        return
+    end
+
+    local text = string.format('```\n%s\n%.2f\n```', choice.name, choice.value)
+    bot.send_message(message.chat.id, text)
+end
--- constants.lua
+++ constants.lua
@@ -4,5 +4,43 @@ Constants = {}
 Constants.CBR_URL = 'http://www.cbr.ru/scripts/XML_daily_eng.asp'
 Constants.TG_BASE_URL = string.format('https://api.telegram.org/bot%s',
                                       os.getenv('TG_BOT_TOKEN'))
+Constants.CODES = [[
+```
+AMD — Armenia Dram
+AUD — Australian Dollar
+AZN — Azerbaijan Manat
+BGN — Bulgarian lev
+BRL — Brazil Real
+BYN — Belarussian Ruble
+CAD — Canadian Dollar
+CHF — Swiss Franc
+CNY — China Yuan
+CZK — Czech Koruna
+DKK — Danish Krone
+EUR — Euro
+GBP — British Pound Sterling
+HKD — Hong Kong Dollar
+HUF — Hungarian Forint
+INR — Indian Rupee
+JPY — Japanese Yen
+KGS — Kyrgyzstan Som
+KRW — South Korean Won
+KZT — Kazakhstan Tenge
+MDL — Moldova Lei
+NOK — Norwegian Krone
+PLN — Polish Zloty
+RON — Romanian Leu
+SEK — Swedish Krona
+SGD — Singapore Dollar
+TJS — Tajikistan Ruble
+TMT — New Turkmenistan Manat
+TRY — Turkish Lira
+UAH — Ukrainian Hryvnia
+USD — US Dollar
+UZS — Uzbekistan Sum
+XDR — SDR
+ZAR — S.African Rand
+```
+]]
 
 return Constants
